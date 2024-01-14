d64890fee671da1d47a89d17a371e9e2379d67d8
sunjuwhan <mathsun0011@gmail.com>
2024-01-14 16:38:08 +0900

    bucket
    put bucket test

--- FastAPI_Server/test/juwhan/NaverCloudSDK/infobucket.py
+++ FastAPI_Server/test/juwhan/NaverCloudSDK/infobucket.py
@@ -5,7 +5,9 @@ endpoint_url = 'https://kr.object.ncloudstorage.com'
 region_name = 'kr-standard'
 access_key = ACCESS_KEY
 secret_key =SECRET_KEY 
-
+bucket_name="test0.1"
+object_name="sample-folder/"
+local_file_path="C:\\Users\\antl\\Documents\\GitHub\\Duck_Farm\\FastAPI_Server\\test\\juwhan\\NaverCloudSDK\\사인2.png"
 class bucket:
     def __init__(self) -> None:
         self.s3=boto3.client(service_name,endpoint_url=endpoint_url,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
@@ -16,14 +18,10 @@ class bucket:
     def show_bucket(self):
         for bucket in self.response.get('Buckets', []):
             print(bucket.get('Name'))
-    
+    def put_bucket(self):
+        self.s3.upload_file(local_file_path,bucket_name,object_name)
 if __name__ == "__main__":
-    #s3 = boto3.client(service_name, endpoint_url=endpoint_url, aws_access_key_id=access_key,
-#                      aws_secret_access_key=secret_key)
-
-    #response = s3.list_buckets()
-
-#    for bucket in response.get('Buckets', []):
-#        print (bucket.get('Name'))
     buc=bucket()
-    buc.show_bucket()
+
+    #buc.show_bucket()
+    buc.put_bucket()
