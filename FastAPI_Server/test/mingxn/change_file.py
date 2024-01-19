5256be45747f2564d24d23dfacc303e6f045529d
김민관 <alsrhks2508@naver.com>
2024-01-19 16:57:36 +0900

    파일 이름 변환기

---
+++ FastAPI_Server/test/mingxn/change_file.py
@@ -0,0 +1,16 @@
+import os
+path = "C:/Users/MZC-USER/Desktop/sample_data/etc"
+def rename_files():
+  i = 1
+
+  
+  for filename in os.listdir(path):
+    dst = str(i) + ".jpg"
+    src = path + '/'  + filename
+    dst = path +'/'  +  dst
+      
+    os.rename(src, dst)
+    i += 1
+
+if __name__ == '__main__':
+  rename_files()
