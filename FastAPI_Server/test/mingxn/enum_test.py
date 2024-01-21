55c10fee7fa49af698d299ecbda8487c854ed7d2
김민관 <alsrhks2508@naver.com>
2024-01-21 17:32:00 +0900

    Enum 테스트

---
+++ FastAPI_Server/test/mingxn/enum_test.py
@@ -0,0 +1,22 @@
+from enum import Enum, auto
+
+class Color(Enum):
+    RED = auto()
+    GREEN = auto()
+    BLUE = auto()
+
+# 열거형 상수 사용 예제
+print(type(Color.RED))     # Color.RED
+print(Color.RED.value)  # 실제 값에 접근
+
+exit()
+
+# 열거형 상수 비교
+if Color.RED == Color.GREEN:
+    print("두 색은 같습니다.")
+else:
+    print("두 색은 다릅니다.")
+
+# 반복문을 통한 열거형 상수 순회
+for color in Color:
+    print(color)
