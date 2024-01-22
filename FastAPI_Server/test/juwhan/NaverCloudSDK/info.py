3a57ab29cff5de75d9616a4290114e125c40157e
sunjuwhan <114124984+sunjuwhan@users.noreply.github.com>
2024-01-22 13:07:31 +0900

    Update info.py

--- FastAPI_Server/test/juwhan/NaverCloudSDK/info.py
+++ FastAPI_Server/test/juwhan/NaverCloudSDK/info.py
@@ -1,4 +1,8 @@
 ACCESS_KEY="3dImIAwFR7HUbEGTmdKl"
 SECRET_KEY="amiKrweWVCFuNFxFmAmDufzkOA8U6PfnjQrWaBy5"
 CDN_DOMAIN="tushdzbddhue22139366.cdn.ntruss.com" #CDN 서비스를 통하기위함 
-MY_CDN_ADDRESS="tushdzbddhue22139366.cdn.ntruss.com"
+# MY_CDN_ADDRESS="tushdzbddhue22139366.cdn.ntruss.com"
+
+# ACCESS_KEY="eeJ2HV8gE5XTjmrBCi48"
+# SECRET_KEY="zAGUlUjXMup1aSpG6SudbNDzPEXHITNkEUDcOGnv"
+# CDN_DOMAIN="kibxopaerykk22247051.cdn.ntruss.com"
