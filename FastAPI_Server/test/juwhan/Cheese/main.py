a8865ed4df1f642a51a2a2554dfe039d3e316a72
sunjuwhan <114124984+sunjuwhan@users.noreply.github.com>
2024-02-20 15:57:35 +0900

    모듈 미분리

--- FastAPI_Server/test/juwhan/Cheese/main.py
+++ FastAPI_Server/test/juwhan/Cheese/main.py
@@ -1,7 +1,18 @@
-from Master_Model import *
+import sample
+class Master:
+    def __init__(self) -> None:
+        self.db=sample.Database()
+        self.local_data=sample.Local()
+
+    def get_user_model(self):
+        return sample.User.User_Model()
+
+    def get_master_model():  #정확한 사용 범위
+        return Master()
+    def get_local_data(self):
+        return self.local_data
+    
 
 
-master=Master.get_master_model()
-user=master.get_user_model()
-user.find_user_uid(134)
-loacl=master.get_local_data()
+if __name__=="__main__":
+    master=Master()
---
+++ FastAPI_Server/test/juwhan/Cheese/sample.py
@@ -0,0 +1,76 @@
+import pymysql
+class Local:
+    def __init__(self) -> None:
+        self.conn=None
+        self.cur=None
+        self.__connect_db() 
+    def __connect_db(self):
+        print("reun __connect_local")
+        self.conn=pymysql.connect(host="127.0.0.1",user="root",password="tjs991101",db='duckfarm',charset='utf8')
+        self.cur=self.conn.cursor()      
+
+    def send_query(self,type,sql):
+        pass
+
+    def __read_data(table,sql):
+        pass
+
+    def __write_data(table,sql):
+        pass
+
+    def __modify_data(table,sql):
+        pass
+
+    def __delete_data(table,sql):
+        pass
+        
+class Database:
+    def __init__(self) -> None:
+        self.conn=None
+        self.cur=None
+        self.__connect_db() 
+    def __connect_db(self):
+        print("reun __connect_local")
+        self.conn=pymysql.connect(host="127.0.0.1",user="root",password="tjs991101",db='duckfarm',charset='utf8')
+        self.cur=self.conn.cursor()      
+
+    def send_query(self,type,sql):
+        pass
+
+    def __read_data(table,sql):
+        pass
+
+    def __write_data(table,sql):
+        pass
+
+    def __modify_data(table,sql):
+        pass
+
+    def __delete_data(table,sql):
+        pass
+
+class User:
+    def __init__(self) -> None:
+        self.uid
+        self.sex
+        self.email
+        self.password
+        self.birthday
+        self.nickname
+    
+    def set_user_data(self,uid,data):
+        pass
+
+class User_Model:
+    def __init__(self) -> None:
+        self.db=None
+
+    
+    def find_user_uid(self,uid):
+        print(uid)
+
+    def get_user_data_as_uid(self,uid,user):
+        pass
+
+    def modify_user_data(self,user):
+        pass
