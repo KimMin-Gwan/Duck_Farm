49bdc533ba370048feaa9693d37f8d891c12076b
sunjuwhan <mathsun0011@gmail.com>
2024-03-04 21:14:37 +0900

    Update database_ma.py

--- FastAPI_Server/monoless_state/src/model/database_ma.py
+++ FastAPI_Server/monoless_state/src/model/database_ma.py
@@ -10,7 +10,7 @@ class Local_Database:
         self.__connect_db() 
     def __connect_db(self):
         print("reun __connect_local")
-        self.conn=pymysql.connect(host="127.0.0.1",user="duck",password="duckfarm1234!",db='duckfarm',charset='utf8')
+        self.conn=pymysql.connect(host="192.168.0.6",user="duck",password="duckfarm1234!",db='duckfarm',charset='utf8')
         self.cur=self.conn.cursor()      
     def send_query(self,type,sql):
         pass
@@ -33,7 +33,7 @@ class Database:
         self.__connect_db() 
     def __connect_db(self):
         print("reun __connect_local")
-        self.conn=pymysql.connect(host="127.0.0.1",user="duck",password="duckfarm1234!",db='duckfarm',charset='utf8')
+        self.conn=pymysql.connect(host="192.168.0.6",user="duck",password="duckfarm1234!",db='duckfarm',charset='utf8')
         self.cur=self.conn.cursor()      
 
     def find_user_email(self,u_email):
