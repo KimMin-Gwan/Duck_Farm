33ab83a63ebb20b4b64f1e61e93c70a6ec0ce25c
sunjuwhan <114124984+sunjuwhan@users.noreply.github.com>
2024-03-05 10:59:52 +0900

    Update model.py

--- FastAPI_Server/monoless_state/src/model/model.py
+++ FastAPI_Server/monoless_state/src/model/model.py
@@ -5,7 +5,7 @@ from src.model.data_format import *
 class User_Model:
     def __init__(self) -> None:
         self.db=Database()
-        self.user = User() # 유저 데이터 초기화 
+        self.user = User(1,(None,None,None,None,None)) # 유저 데이터 초기화 
         self.otp = 0
 
     # 유저 정보 리턴 
