41c10cee02872dff4230d175bb3812619387f8d3
sunjuwhan <114124984+sunjuwhan@users.noreply.github.com>
2024-03-05 10:58:50 +0900

    Update data_format.py

--- FastAPI_Server/monoless_state/src/model/data_format.py
+++ FastAPI_Server/monoless_state/src/model/data_format.py
@@ -5,16 +5,20 @@ import json
 # 3. mutator중 하나로 한번에 모든 내부 데이터 만들 수 있도록 설정
 class User:
     def __init__(self) -> None:
-        self.uid
-        self.sex
-        self.email
-        self.password
-        self.birthday
-        self.nickname
+        self.__uid
+        self.__sex
+        self.__email
+        self.__password
+        self.__birthday
+        self.__nickname
     
     def set_user_data(self,uid,data):
-        pass
-
+        self.__uid=uid
+        self.__sex=data[0]
+        self.__email=data[1]
+        self.__password=data[2]
+        self.__birthday=data[3]
+        self.__nickname=data[4]
 
 # 이건 아직 작성 안해도됨
 class Profile:
