bf9a0a92a538ecedb84892895dc76ced7de00815
sunjuwhan <mathsun0011@gmail.com>
2024-03-05 12:36:22 +0900

    Update test_main.py

--- FastAPI_Server/monoless_state/test_main.py
+++ FastAPI_Server/monoless_state/test_main.py
@@ -3,4 +3,6 @@ from src.model.model import *
 
 if __name__=="__main__":
     user=User_Model()
-    user.find_email("mathsun0011@gmail.com")
+    email=input("input email")
+    password=input("input passwod")
+    user.find_email(email)
