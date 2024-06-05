6d254d9de037384333ca9f23ea4eaeceac01cf15
seyoungse <choiseyoung0531@naver.com>
2024-06-05 19:05:04 +0900

    Hello

--- Server/test/test.py
+++ Server/test/test.py
@@ -1,17 +1,1 @@
-
-class Configure_File_Reader:
-    def __init__(self) -> None:
-        self.host = ""
-        self.port = 0000
-        self.version = ""
-
-    def extract_host_port(self, file_path="./configure.txt"):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            for line in file:
-                if line.startswith('HOST'):
-                    self.host = line.split('=')[1].strip()
-                elif line.startswith('PORT'):
-                    self.port = int(line.split('=')[1].strip())
-                elif line.startswith('VERSION'):
-                    self.version = line.split('=')[1].strip()
-        
+print("Hello")
