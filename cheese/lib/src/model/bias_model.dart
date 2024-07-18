dbc8d51cb0ff4b7371e2eb7385b3249b64f60be9
김민관 <alsrhks2508@naver.com>
2024-07-19 04:26:24 +0900

    Update bias_model.dart

--- cheese/lib/src/model/bias_model.dart
+++ cheese/lib/src/model/bias_model.dart
@@ -1,18 +1,1 @@
 
-class BiasModel{
-
-}
-
-class Bias{
-  String bid = '';
-  String bname = '';
-
-  void set_bid(bid){
-    this.bid = bid;
-  }
-  void set_bname(bname){
-    this.bname= bname;
-  }
-
-
-}
