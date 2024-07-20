f3bbd7bf2a330785e7c489753d7dcf8af5241cfa
BBangGy <hyeongyu-98@naver.com>
2024-07-20 23:51:21 +0900

    bias_model.dart
    biasListModel을 다시 재구성 했슴

--- cheese/lib/src/model/bias_model.dart
+++ cheese/lib/src/model/bias_model.dart
@@ -1,20 +1,24 @@
 import 'package:cheese/src/bloc/core_bloc/core_state.dart';
-
+import 'package:cheese/src/data_domain/data_domain.dart';
 class BiasListModel{
-  final String bid;
-  final String bname;
+  final List<Bias> biasList;
 
 
   BiasListModel({
-    required this.bid,
-    required this.bname,
+    required this.biasList,
   });
 
   factory BiasListModel.fromJson(Map data){
     print(data['body']);
-    return BiasListModel(
-        bid: data['body']['bias_list']['bid'],
-        bname: data['body']['bias_list']['bname'],
-    );
+    List<Bias> biasList = [];
+    List<Bias> order = data['body']['bias_order'];
+    for ( var  biasData in data['body']['bias_list'] ) {
+      Bias bias = Bias();
+      bias.bid = biasData['bid'];
+      bias.bname = biasData['bname'];
+      biasList.add(bias);
+      biasList.add(data['body']['bias_order']);
+    }
+    return BiasListModel(biasList);
   }
 }
